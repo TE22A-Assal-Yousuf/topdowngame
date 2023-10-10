09aeee3b67cde86d59495602aaf28bbbed997624
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2023-10-10 11:05:07 +0200

    beta1

--- topdowngame/Program.cs
+++ topdowngame/Program.cs
@@ -1,8 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+using System.Numerics;
+
+Raylib.InitWindow(800,600, "cringe");
+
+int x = 0;
+int y = 12;
+int FPS = 1;
+
+Vector2 position = new Vector2(400, 300);
 
 
+while(!Raylib.WindowShouldClose())
+{
+
+    x++;
+    y++;
+
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.VIOLET);
+    Raylib.DrawCircle(x, y, 30, Color.BLACK);
+
+    if (y == 300){
+        y = 12;
+        x = 0;
+        FPS *= FPS;
+    }
+
+    Raylib.SetTargetFPS(FPS);
 
 
-
-Console.ReadLine();
+    Raylib.EndDrawing();
+}
